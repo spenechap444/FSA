20737f1daa06a926f7149d6b5d07c2cd5be70af4
Spencer Chapman <chappydev4@gmail.com>
2023-11-20 21:18:35 -0500

    Storing balance sheet initial implementation
    contains both application and database changes

--- FSA_Coordinator/src/main/consumer/AlphaVantageConsumer.py
+++ FSA_Coordinator/src/main/consumer/AlphaVantageConsumer.py
@@ -1,10 +1,14 @@
 #This file is meant for contacting the AlphaVantage API to consume Data
 import requests
+import datetime
+import re
+
 
 class API:
-    def __init__(self, ticker, api_key):
+    def __init__(self, ticker, api_key, url):
         self.__ticker = ticker
         self.__api_key = api_key
+        self.__url = url
         self.output = None
 
     #private method
@@ -18,30 +22,54 @@ class API:
     #private method
     def parse(self):
         # formatting the output from API to be in format for DB
+        omitted_cols = ['capitalLeaseObligations', 'longTermDebt', 'otherCurrentLiabilities', 'otherNonCurrentLiabilities']
         ticker = self.output['symbol']
+        now = datetime.datetime.now()
         reports = {}
+        #prepping the data for storing in the database
         for statement in self.output['quarterlyReports']:
+            statement_values = [] #initializing empty list for fiscal period data
+            statement_values.append(ticker)
             fiscalDateEnd = statement['fiscalDateEnding']
-            reports[(ticker, fiscalDateEnd)] = statement
-            #symbol and fiscal date end are primary keys in all 3 tables
+            for item in statement:
+                if item not in omitted_cols:
+                    #date
+                    if re.match('[0-9]{4}\-[0-9]{2}\-[0-9]{2}', statement[item]):
+                        dt_split = statement[item].split('-')
+                        statement[item] = datetime.date(int(dt_split[0]), int(dt_split[1]), int(dt_split[2]))
+                    #number
+                    elif re.match('[0-9]', statement[item]):
+                        statement[item] = int(statement[item])
+                    #null data
+                    elif statement[item] == "None":
+                        statement[item] = None
 
+                    # adding cleaned data to a list
+                    statement_values.append(statement[item])
+
+                # Adding values to the end of the report
+            statement_values.append('AP001')  # app ID
+            statement_values.append(now)  # time stored in table
+
+            # storing list as a tuple in dictionary
+            reports[(ticker, fiscalDateEnd)] = tuple(statement_values)
+            #symbol and fiscal date end are primary keys in all 3 tables
         self.output = reports
+
 
     #public~ service level method
     def api_fetch(self, statement):
         # based on input, API request will fetch statement of choice
         if statement == 'IS':
             report = 'INCOME_STATEMENT'
-            url = f'https://www.alphavantage.co/query?function={report}&symbol={self.__ticker}&apikey={self.__api_key}'
         elif statement == 'BS':
             report = 'BALANCE_SHEET'
-            url = f'https://www.alphavantage.co/query?function={report}&symbol={self.__ticker}&apikey={self.__api_key}'
         elif statement == 'CF':
             report = 'CASH_FLOW'
         else:
             print(
                 'Invalid statement ~ Available options:\n\tIS: Income Statement\n\tBS: Balance Sheet\n\tCF: Statement of Cash Flows')
 
-        url = f'https://www.alphavantage.co/query?function={report}&symbol={self.__ticker}&apikey={self.__api_key}'
-        self.request_url(url)
+        url_instance = self.__url.format(report, self.__ticker, self.__api_key)
+        self.request(url_instance)
         self.parse()
--- FSA_Coordinator/src/main/main.py
+++ FSA_Coordinator/src/main/main.py
@@ -2,15 +2,32 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import Utilities.Query as db
+import consumer.AlphaVantageConsumer as av
+import os
+import json
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def testing():
+    #os.path.dirname(__file__) gets directory of current script
+    resource_path = os.path.join(os.path.dirname(__file__),'Utilities', 'resources','creds.json')
+    with open(resource_path,'r') as f:
+        creds = json.load(f)
+
+    cnn = db.DB_cnn(creds['DB'])
+    db_conn = cnn.open_cnn
+
+    BS = av.API('TSLA', creds['Alpha']['key'], creds['Alpha']['fs_url'])
+    BS.api_fetch('BS')
+    # cnn.store_data(BS.output)
+    for item in BS.output:
+        cnn.store_data(BS.output[item])
+        print(BS.output[item])
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    testing()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
